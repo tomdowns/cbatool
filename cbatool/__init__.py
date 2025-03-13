3a1a9cef0b4151aa421e828156cc060b4bf0434a
Tom Downs <tomdowns1986@gmail.com>
2025-03-13 18:32:02 +0900

    Fixing functionality - analysis classes import

--- cbatool/__init__.py
+++ cbatool/__init__.py
@@ -8,7 +8,8 @@ __author__ = 'Your Name'
 # Import main classes to make them available at package level
 try:
     from .core.data_loader import DataLoader
-    from .core.analyzer import Analyzer
+    from .core.depth_analyzer import DepthAnalyzer
+    from .core.position_analyzer import PositionAnalyzer
     from .core.visualizer import Visualizer
 except ImportError as e:
     print(f"Warning: Could not import main classes: {e}")
