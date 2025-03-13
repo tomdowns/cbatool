e6130202c09267ac126dfc6917d6457ef3382624
Tom Downs <tomdowns1986@gmail.com>
2025-03-13 18:27:33 +0900

    Fixing Functionality - standardised analyzer class imported in core/__init__

--- cbatool/core/__init__.py
+++ cbatool/core/__init__.py
@@ -4,15 +4,28 @@ Core components for CBAtool including data loading, analysis, and visualization.
 
 # First, make sure these module files exist in this directory
 try:
+    # Import modules
+    from . import base_analyzer
+    from . import depth_analyzer
+    from . import position_analyzer
     from . import data_loader
     from . import analyzer
     from . import visualizer
+    from . import position_visualizer
     
-    # Then try to expose the classes
-    from ..core.data_loader import DataLoader
-    from ..core.analyzer import Analyzer
-    from ..core.visualizer import Visualizer
+    # Then expose classes
+    from .base_analyzer import BaseAnalyzer
+    from .depth_analyzer import DepthAnalyzer
+    from .position_analyzer import PositionAnalyzer
+    from .data_loader import DataLoader
+    from .visualizer import Visualizer
     
-    __all__ = ['DataLoader', 'Analyzer', 'Visualizer']
+    __all__ = [
+        'BaseAnalyzer', 
+        'DepthAnalyzer', 
+        'PositionAnalyzer', 
+        'DataLoader', 
+        'Visualizer'
+    ]
 except ImportError as e:
     print(f"Warning: Could not import core modules: {e}")
