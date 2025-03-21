ee1b266d39d2ff98a4b30c47480ccc514b97b8f8
Tom Downs <tomdowns1986@gmail.com>
2025-03-21 13:56:34 +0900

    chore(config): enhance commitlint rules to match project standards
    Add mandatory component scopes, expand allowed types, and improve
    formatting rules to better align with documented commit message format.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2, 
+      'always', 
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']
+    ],
+    'scope-enum': [
+      2,
+      'always',
+      ['analyzer', 'visualizer', 'ui', 'utils', 'config', 'report', 'core', 'data', 'docs', 'test']
+    ],
+    'scope-empty': [2, 'never'],
+    'subject-case': [2, 'never', ['start-case', 'pascal-case']],
+    'subject-min-length': [2, 'always', 5],
+    'body-max-line-length': [1, 'always', 100],
+    'footer-leading-blank': [1, 'always']
+  }
 };
